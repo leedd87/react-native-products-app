87e03a044144b35c47abc390cccc1ee257fb6df1
leedd87 <leedav87@gmail.com>
2023-02-15 08:52:49 -0300

    add login component

---
+++ src/components/Background.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Background = () => {
+    return (
+        <View
+            style={{
+                position: 'absolute',
+                backgroundColor: '#5856D6',
+                top: -350,
+                width: 1000,
+                height: 1200,
+                transform: [
+                    { rotate: '-79deg' }
+                ]
+            }}
+        />
+    )
+}
+
+export default Background
