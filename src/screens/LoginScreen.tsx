78cd40b17201c571f75b453748365b76566ed7ac
leedd87 <leedav87@gmail.com>
2023-02-15 09:27:59 -0300

    add styling login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { Platform, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import Background from '../components/Background'
 import WhiteLogo from '../components/WhiteLogo'
 import { loginStyles } from '../theme/loginTheme'
@@ -9,15 +9,59 @@ export const LoginScreen = () => {
         <>
             <Background />
 
-            <WhiteLogo />
+            <View style={loginStyles.formContainer}>
 
-            <Text style={loginStyles.title}>Login</Text>
-            <Text style={loginStyles.label}>Email</Text>
-            <TextInput
-                placeholder='Ingrese su email'
-                placeholderTextColor='rgba(255,255,255,0.4)'
-                keyboardType='email-address'
-            />
+
+                <WhiteLogo />
+
+                <Text style={loginStyles.title}>Login</Text>
+                <Text style={loginStyles.label}>Email</Text>
+                <TextInput
+                    placeholder='Ingrese su email'
+                    placeholderTextColor='rgba(255,255,255,0.4)'
+                    keyboardType='email-address'
+                    underlineColorAndroid='white'
+                    style={[loginStyles.inputField,
+                    Platform.OS === 'ios' && loginStyles.inputFieldIOS
+                    ]}
+                    selectionColor='white'
+                    //TODO: onchange,value
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                />
+                <Text style={loginStyles.label}>Password</Text>
+                <TextInput
+                    placeholder='*******'
+                    placeholderTextColor='rgba(255,255,255,0.4)'
+                    keyboardType='email-address'
+                    underlineColorAndroid='white'
+                    style={[loginStyles.inputField,
+                    Platform.OS === 'ios' && loginStyles.inputFieldIOS
+                    ]}
+                    selectionColor='white'
+                    //TODO: onchange,value
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                />
+
+                <View style={loginStyles.buttonContainer}>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={loginStyles.button}
+                    >
+                        <Text style={loginStyles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={loginStyles.newUserContainer}>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        onPress={() => console.log('press')}
+                    >
+                        <Text style={loginStyles.buttonText}>Nueva cuenta</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </>
     )
 }
