7718640ebb181dd3804ca3be70c80219aebebddc
leedd87 <leedav87@gmail.com>
2023-02-23 11:46:00 -0300

    add logOut button

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -79,7 +79,10 @@ export const AuthProvider = ({ children }: any) => {
 
 
     };
-    const logOut = () => { };
+    const logOut = async () => {
+        await AsyncStorage.removeItem('token');
+        dispatch({ type: 'logout' })
+    };
     const removeError = () => {
 
         dispatch({
