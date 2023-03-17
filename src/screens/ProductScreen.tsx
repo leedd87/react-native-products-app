93cc2d1258e1cae0103adc9f4dfd8b59c0373889
leedd87 <leedav87@gmail.com>
2023-03-17 10:30:53 -0300

    add products navigation

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import { StackScreenProps } from '@react-navigation/stack'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
+import { ProductsStackParams } from '../navigator/ProductsNavigator'
 
-export const ProductScreen = () => {
+
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductScreen'> { }
+
+export const ProductScreen = ({ navigation, route }: Props) => {
+
+
+    const { name = '', id } = route.params
+
+    useEffect(() => {
+        navigation.setOptions({
+            title: (name) ? name : 'Nuevo Producto',
+
+        })
+    }, [])
+
+
     return (
         <View>
-            <Text>
-
-                ProductScreen
-            </Text>
+            <Text>{id} {name}</Text>
         </View>
     )
 }
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,10 +1,33 @@
-import React, { useContext } from 'react'
+import { StackScreenProps } from '@react-navigation/stack'
+import React, { useContext, useEffect } from 'react'
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { ProductsContext } from '../context/ProductsContext'
+import { ProductsStackParams } from '../navigator/ProductsNavigator'
 
-export const ProductsScreen = () => {
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> { }
+
+export const ProductsScreen = ({ navigation }: Props) => {
 
     const { products, loadProducts } = useContext(ProductsContext)
+
+    useEffect(() => {
+
+        navigation.setOptions({
+
+            headerRight: () => (
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={{ marginRight: 10, }}
+                    onPress={() => navigation.navigate('ProductScreen', {})}
+                >
+                    <Text>Agregar</Text>
+                </TouchableOpacity>
+            )
+
+        })
+
+    }, [])
+
 
     return (
         <View style={{ flex: 1, marginHorizontal: 10 }}>
@@ -13,7 +36,12 @@ export const ProductsScreen = () => {
                 keyExtractor={p => p._id}
                 renderItem={(({ item }) => (
                     <TouchableOpacity
-                        activeOpacity={0.8}>
+                        activeOpacity={0.8}
+                        onPress={() => navigation.navigate('ProductScreen', {
+                            id: item._id,
+                            name: item.nombre
+                        })}
+                    >
 
                         <Text style={styles.productName}>{item.nombre}</Text>
                     </TouchableOpacity>
