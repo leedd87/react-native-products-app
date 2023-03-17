2e922cbc3b7edb987af451e3c16fca82541c8fe7
leedd87 <leedav87@gmail.com>
2023-03-17 10:52:43 -0300

    add styling to product screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import { ProductsStackParams } from '../navigator/ProductsNavigator'
 
 
@@ -20,8 +20,62 @@ export const ProductScreen = ({ navigation, route }: Props) => {
 
 
     return (
-        <View>
-            <Text>{id} {name}</Text>
+        <View style={styles.container}>
+            <ScrollView>
+                <Text style={styles.label}>Nombre del producto:</Text>
+                <TextInput
+                    placeholder='Producto'
+                    style={styles.textInput}
+                />
+
+                {/*Picker/Selector */}
+                <Text style={styles.label}>Categoria:</Text>
+
+                <Button
+                    title='Guardar'
+                    onPress={() => { }}
+                    color='#5856D6'
+                />
+
+                <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+                    <Button
+                        title='Camara'
+                        onPress={() => { }}
+                        color='#5856D6'
+                    />
+                    <View style={{ width: 10 }} />
+                    <Button
+                        title='Galeria'
+                        onPress={() => { }}
+                        color='#5856D6'
+                    />
+
+                </View>
+
+                <Text>{id} {name}</Text>
+
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10,
+        marginHorizontal: 20,
+    },
+    label: {
+        fontSize: 18,
+    },
+    textInput: {
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 20,
+        borderColor: 'green',
+        height: 45,
+        marginTop: 5,
+        marginBottom: 10
+    }
+})
