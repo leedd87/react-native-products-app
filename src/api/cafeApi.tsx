8b5b8f0fe5575f891a5b9a0725c342b98988a2e4
leedd87 <leedav87@gmail.com>
2023-03-22 10:10:01 -0300

    change url to localhost

--- src/api/cafeApi.tsx
+++ src/api/cafeApi.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const baseURL = 'https://cafe-app-native-production.up.railway.app/api';
+//const baseURL = 'https://cafe-app-native-production.up.railway.app/api';
+const baseURL = 'http://192.168.1.2:8080/api'
 
 const cafeApi = axios.create({ baseURL });
 
